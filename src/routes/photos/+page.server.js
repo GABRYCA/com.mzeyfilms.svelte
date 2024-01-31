c68125f498f1d8300a7707698ff3fa82d328938f
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2024-01-31 14:42:59 +0100

    Changelogs: - Performance improvements. - Minor changes.
    TODO:
    - Add loader for upload files, so it doesn't feel too static.
    - Generally complete the website.

--- src/routes/photos/+page.server.js
+++ src/routes/photos/+page.server.js
@@ -1,39 +1,24 @@
 import path from "path";
 import fs from "fs/promises";
 
-export async function load(){
-
+export async function load() {
     const production = true;
-
-    // Get a list of folders inside 'static' folder
     const pathFolders = path.resolve('static/', 'photos');
-
-    // Read folders names
     let folders = await fs.readdir(pathFolders);
-    let foldersWithImages = [];
-
-    // Remove from folder array the folder with name 'old'
     const index = folders.indexOf('old');
     if (index > -1) {
         folders.splice(index, 1);
     }
 
-    // Get src of each image and group them by folder in the foldersWithImages array object
-    for (let i = 0; i < folders.length; i++) {
-        const pathFolder = path.resolve('static/', 'photos', folders[i]);
-        const images = await fs.readdir(pathFolder);
-        for (let j = 0; j < images.length; j++) {
-            if (production){
-                images[j] = '\\static\\photos\\' + folders[i] + '\\' + images[j];
-            } else {
-                images[j] = '\\photos\\' + folders[i] + '\\' + images[j];
-            }
-        }
-        foldersWithImages.push({
-            name: folders[i],
+    const foldersWithImages = await Promise.all(folders.map(async folder => {
+        const pathFolder = path.resolve('static/', 'photos', folder);
+        let images = await fs.readdir(pathFolder);
+        images = images.map(image => production ? `\\static\\photos\\${folder}\\${image}` : `\\photos\\${folder}\\${image}`);
+        return {
+            name: folder,
             images: images
-        });
-    }
+        };
+    }));
 
     return {
         body: {
--- src/routes/videos/+page.server.js
+++ src/routes/videos/+page.server.js
@@ -2,25 +2,15 @@ import path from "path";
 import fs from "fs/promises";
 
 export async function load() {
-
     const production = true;
-
-    // Get a list of videos inside 'static/videos' folder, excluding the ones in the subfolder 'old'
     const pathFolders = path.resolve('static/', 'videos');
-    const videos = await fs.readdir(pathFolders);
+    let videos = await fs.readdir(pathFolders);
     const index = videos.indexOf('old');
     if (index > -1) {
         videos.splice(index, 1);
     }
 
-    // Get static vite path of each video and save it into videos array
-    for (let i = 0; i < videos.length; i++) {
-        if (production){
-            videos[i] = '\\static\\videos\\' + videos[i];
-        } else {
-            videos[i] = '\\videos\\' + videos[i];
-        }
-    }
+    videos = videos.map(video => production ? `\\static\\videos\\${video}` : `\\videos\\${video}`);
 
     return {
         body: {

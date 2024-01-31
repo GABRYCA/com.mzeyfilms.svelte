054e4d57c73944d30685a0f9aaca28f2fa3a467f
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2024-01-31 13:23:59 +0100

    Changelogs: - Added some vertical padding to images. - Minor changes.
    TODO:
    - Add loader for upload files, so it doesn't feel too static.
    - Generally complete the website.

--- src/lib/components/UserPhoto.svelte
+++ src/lib/components/UserPhoto.svelte
@@ -2,7 +2,7 @@
     export let src;
 </script>
 
-<div class="col-12 col-md-6 mt-3 my-md-auto">
+<div class="col-12 col-md-6 mt-3 my-md-auto py-md-2">
     <a href="{src}" target="_blank"><img src="{src}" class="img-fluid rounded-4" alt="Gallery Content"></a>
 </div>
 

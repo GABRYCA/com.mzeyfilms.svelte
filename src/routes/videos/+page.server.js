bfa4f83679163f88657d684e3e8c4165895f868f
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2025-01-07 00:50:37 +0100

    Changelogs: - Optimized videos loading section. - Minor changes.

--- src/routes/videos/+page.server.js
+++ src/routes/videos/+page.server.js
@@ -1,17 +1,63 @@
-import { PRIVATE_POCKETBASE_EMAIL, PRIVATE_POCKETBASE_PASSWORD } from '$env/static/private';
-import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import {PRIVATE_POCKETBASE_EMAIL, PRIVATE_POCKETBASE_PASSWORD} from '$env/static/private';
+import {PUBLIC_POCKETBASE_URL} from '$env/static/public';
 import PocketBase from "pocketbase";
+
+const pageSize = 2;
+
+async function fetchVideos(page, pb) {
+    return await pb.collection('videos').getList(page, pageSize, {
+        sort: '+created',
+    });
+}
 
 export async function load() {
 
     const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
     await pb.admins.authWithPassword(PRIVATE_POCKETBASE_EMAIL, PRIVATE_POCKETBASE_PASSWORD);
 
-    const videos = await pb.collection('videos').getFullList();
+    //const videos = await pb.collection('videos').getFullList();
+    const videos = await fetchVideos(1, pb);
 
     return {
-        videos: videos,
+        videos: videos.items,
         title: 'MZEYFILMS - Videos',
         description: 'MZEFILMS Videos. Watch my latest featured videos.',
+    }
+}
+
+export const actions = {
+    videos: async ({ request }) => {
+        const formData = Object.fromEntries(await request.formData());
+        const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+        await pb.admins.authWithPassword(PRIVATE_POCKETBASE_EMAIL, PRIVATE_POCKETBASE_PASSWORD);
+
+        const page = formData.page;
+
+        if (page === null || isNaN(page) || page === 0) {
+            return {
+                status: 400,
+                body: {
+                    error: 'Invalid page number',
+                }
+            }
+        }
+
+        let videos = await fetchVideos(page, pb);
+
+        if (videos.items.length === 0) {
+            return {
+                status: 404,
+                body: {
+                    error: 'No videos found',
+                }
+            }
+        }
+
+        return {
+            status: 200,
+            body: {
+                videos: videos.items,
+            }
+        }
     }
 }
--- src/routes/videos/+page.svelte
+++ src/routes/videos/+page.svelte
@@ -1,15 +1,57 @@
 <script>
     import {onMount} from "svelte";
     import UserVideo from "$lib/components/UserVideo.svelte";
+    import {deserialize} from "$app/forms";
     let { data } = $props();
-    const { videos } = data;
+    let { videos } = $state(data);
+    let isFetching = false;
+    let page = 1;
+    let loadedAllPages = false;
+    let y = $state(0);
 
     onMount(() => {
         document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((element) => {
             new bootstrap.Tooltip(element);
         });
     });
+
+    async function loadMoreVideos() {
+        if (isFetching) return;
+        isFetching = true;
+
+        page += 1;
+
+        const formData = new FormData();
+        formData.append('page', page);
+
+        const response = await fetch('?/videos', {
+            method: 'POST',
+            body: formData
+        });
+
+        const results = deserialize(await response.text());
+        if (results.type === 'success') {
+            if (results.data.status === 200) {
+                if (results.data.body.videos.length > 0) {
+                    videos.push(...results.data.body.videos);
+                }
+            } else if (results.data.status === 404) {
+                loadedAllPages = true;
+            }
+        }
+
+        isFetching = false;
+    }
+
+    function handleScroll(event) {
+        const target = event.target.body;
+        if (y >= target.clientHeight / 4) {
+            if (!loadedAllPages) loadMoreVideos();
+        }
+    }
 </script>
+
+<svelte:window onscroll={handleScroll} bind:scrollY={y}/>
 
 <div class="container-xxl" data-aos="zoom-in" data-aos-duration="400">
     <div class="row align-items-center" style="min-height: 78vh">

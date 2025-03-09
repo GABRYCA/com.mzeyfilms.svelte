7228a5f64fc616ec83c38b4ec03ddf5277ee0c6c
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2025-03-09 22:13:04 +0100

    Changelogs: - Updated Pocketbase handler. - Minor changes.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -15,6 +15,16 @@ export const handle = async ({ event, resolve }) => {
         pb_initiated = pb;
     }
 
+    if (pb_initiated && !pb_initiated.authStore.isValid) {
+        console.log(" [PocketBase] re-init");
+        pb_initiated.authStore.clear();
+
+        const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+        await pb.collection('_superusers').authWithPassword(PRIVATE_POCKETBASE_EMAIL, PRIVATE_POCKETBASE_PASSWORD);
+
+        pb_initiated = pb;
+    }
+
     if (!event.locals.pb) event.locals.pb = pb_initiated;
 
     return resolve(event);

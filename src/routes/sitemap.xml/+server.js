177e7a83c2b995938b6b4c3731e6e70257342bba
AnonymousGCA <anonymousgca@anonymousgca.eu>
2025-04-16 00:31:36 +0200

    Changelogs: - Enabled prerendering on a few selected pages. - Minor changes.

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,5 +1,6 @@
-// /src/routes/sitemap.xml/+server.js
 import * as sitemap from 'super-sitemap';
+
+export const prerender = true;
 
 export const GET = async () => {
     return await sitemap.response({
